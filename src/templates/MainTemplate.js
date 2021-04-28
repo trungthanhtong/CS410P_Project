5688e7da6f4750696ffe9db7f43732af47ede16f
Thanh Tong <trungthanhtong@gmail.com>
2021-04-28 10:21:31 -0700

    update MainTemplate

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import SideMenu from "../components/SideMenu";
+import { Layout } from "antd";
 
+const { Header, Content } = Layout;
 
 export default function MainTemplate(props) {
     let { Component, ...restRoute } = props;
@@ -11,9 +13,31 @@ export default function MainTemplate(props) {
             {...restRoute}
             render={(propsRoute) => {
                 return (
-                    <div>
-                       <SideMenu/>
-                    </div>
+                    <Layout>
+                        <SideMenu/>
+                        <Layout
+                            className="site-layout"
+                            style={{ marginLeft: 200 }}
+                        >
+                            <Header
+                                className="site-layout-background"
+                                style={{ padding: 0 }}
+                            />
+                            <Content
+                                style={{
+                                    margin: "24px 16px 0",
+                                    overflow: "initial",
+                                }}
+                            >
+                                <div
+                                    className="site-layout-background"
+                                    style={{ padding: 24 }}
+                                >
+                                  <Component {...restRoute}/>
+                                </div>
+                            </Content>
+                        </Layout>
+                    </Layout>
                 );
             }}
         />
