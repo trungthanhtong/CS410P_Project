413896d09c37e1659e2a5ade9ee46176953c9e41
Maeve Hoffer <dhoffer@pdx.edu>
2021-05-18 21:57:37 -0700

    Fetch data from API for SteamStat component

--- src/pages/SteamStats/SteamStat.js
+++ src/pages/SteamStats/SteamStat.js
@@ -1,9 +1,23 @@
+import axios from 'axios'
 import React from 'react'
 
 export default function SteamStat() {
+    let axios = require('axios');
+
+    var popular = axios.get("http://steamtistics-be.herokuapp.com/game/most-played/5")
+        .then(response => console.log(response))
+        .then(function format(response) {
+            let results = document.createElement('div');
+            results.id = "most_popular";
+            
+
+        })
+    
+    
+
     return (
         <div>
-            steam stat
+            <h1>Most Played Games</h1>
         </div>
     )
 }
