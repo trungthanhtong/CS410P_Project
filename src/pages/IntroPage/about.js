18abe36ac0753a1eb280bc0a92872108a272535c
Maeve Hoffer <dhoffer@pdx.edu>
2021-06-04 16:50:07 -0700

    Add more info to about page

--- src/pages/IntroPage/about.js
+++ src/pages/IntroPage/about.js
@@ -16,9 +16,8 @@ export default class About extends Component{
                      alt="steamstats_about_title"
                 />
                 <div className="mobile_view">
-                <h4 className="text-white"><b>Purpose:</b> We wish to share information about game in 
-                which will help in your decision when planning to find or buy a new game on the Steam gaming
-                platform.</h4>
+                <h4 className="text-white"><b>Purpose:</b> We developed this dashboard to share information about the top reviewed games on Steam 
+                in order to help you find or buy a new game on the Steam gaming platform.</h4>
     
                 <h5 className="text-white mt-5">We provide a range of information about game reviews on the Steam platform
                 based on many criteria to provide information about the most played games and most reviewed games.</h5>
@@ -26,6 +25,9 @@ export default class About extends Component{
                 <h5 className="text-white mt-5">Game reviews play a crucial part in deciding whether or not to get 
                 a particular game. Therefore, we also provide information about reviews. This includes information about
                 the funniest and most helpful reviews found on Steam.</h5>
+
+                <h5 className="text-white mt-5">To get started, you can press Start Tutorial to get more information about how to navigate the dashboard.
+                If you are ready to see the dashboard, just press Get Started!</h5>
                 </div>
     
                 <button
